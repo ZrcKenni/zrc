57d0ca57b13b4db8aea13778165891218cee13ce
ZrcKenni <zrcken@outlook.com>
2024-07-26 06:04:57 +0530

    Update rpc.sh

--- path/to/rpc.sh
+++ path/to/rpc.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Set up the folder
+echo "installing node.js"
+pkg update -y && pkg upgrade -y
+pkg install nodejs -y
+
 FOLDER_NAME="zrcs-dsc-rpc"
 echo "Creating folder $FOLDER_NAME..."
 mkdir -p $FOLDER_NAME
-
-# Navigate to the folder
 cd $FOLDER_NAME
 
-# Download index.js
 echo "Downloading index.js..."
 curl -O https://www.zrcs.com/path/to/index.js
 
-# Initialize a new Node.js project and install dependencies
 echo "Initializing Node.js project and installing dependencies..."
 npm init -y
 npm install discord.js-selfbot-v13 readline-sync colors
 
-# Run the script
 echo "Setup complete. You can now run the script using: node index.js"
+
+cd zrcs-rpc
